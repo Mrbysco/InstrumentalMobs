c43875c878b4f0ee9dbfc41e83e8107a92248391
Mrbysco <Heeybysco@live.nl>
2018-07-31 23:18:00 +0200

    For now make the drum non-placeable

--- src/main/java/com/Mrbysco/InstrumentalMobs/blocks/DrumBlock.java
+++ src/main/java/com/Mrbysco/InstrumentalMobs/blocks/DrumBlock.java
@@ -4,29 +4,20 @@ import java.util.Random;
 
 import com.Mrbysco.InstrumentalMobs.InstrumentalMobs;
 import com.Mrbysco.InstrumentalMobs.Reference;
-import com.Mrbysco.InstrumentalMobs.config.InstrumentalConfigGen;
 import com.Mrbysco.InstrumentalMobs.init.InstrumentalItems;
-import com.Mrbysco.InstrumentalMobs.init.InstrumentalSounds;
-import com.Mrbysco.InstrumentalMobs.utils.InstrumentHelper;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
-import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockFaceShape;
-import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
 
 public class DrumBlock extends Block{
     protected static final AxisAlignedBB AABB = new AxisAlignedBB(0.0625D, 0.0D, 0.0625D, 0.9375D, 0.75D, 0.9375D);
-    public static final PropertyInteger DAMAGE = PropertyInteger.create("damage", 0, 15);
 
 	public DrumBlock(String registryName) {
 		super(Material.CLOTH);
@@ -36,20 +27,7 @@ public class DrumBlock extends Block{
 		this.setUnlocalizedName(Reference.MOD_PREFIX + registryName.replaceAll("_", ""));
 		this.setRegistryName(registryName);
 		this.setCreativeTab(InstrumentalMobs.instrumentalTab);
-		
-		this.setDefaultState(this.blockState.getBaseState().withProperty(DAMAGE, 0));
-	}
-	
-	@Override
-	public void onBlockClicked(World worldIn, BlockPos pos, EntityPlayer playerIn) {
-		playerIn.playSound(InstrumentalSounds.single_drum_sound, 1F, 1F);
-		if(InstrumentalConfigGen.general.mobsReact)
-		{
-			InstrumentHelper.instrumentDamage(worldIn, playerIn);
 		}
-		
-		super.onBlockClicked(worldIn, pos, playerIn);
-	}
 	
 	@Override
 	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
@@ -81,26 +59,4 @@ public class DrumBlock extends Block{
 	public boolean isOpaqueCube(IBlockState state) {
 		return false;
 	}
-	
-	@Override
-	public int damageDropped(IBlockState state)
-    {
-        return ((Integer)state.getValue(DAMAGE)).intValue();
-    }
-	
-	public IBlockState getStateFromMeta(int meta)
-    {
-		 return getDefaultState().withProperty(DAMAGE, meta);    
-    }
-
-    public int getMetaFromState(IBlockState state)
-    {
-    	 return state.getValue(DAMAGE);
-    }
-    
-	@Override
-	protected BlockStateContainer createBlockState()
-    {
-        return new BlockStateContainer(this, new IProperty[] {DAMAGE});
-    }
 }
--- src/main/java/com/Mrbysco/InstrumentalMobs/items/DrumInstrument.java
+++ src/main/java/com/Mrbysco/InstrumentalMobs/items/DrumInstrument.java
@@ -2,28 +2,18 @@ package com.Mrbysco.InstrumentalMobs.items;
 
 import com.Mrbysco.InstrumentalMobs.InstrumentalMobs;
 import com.Mrbysco.InstrumentalMobs.Reference;
-import com.Mrbysco.InstrumentalMobs.blocks.DrumBlock;
 import com.Mrbysco.InstrumentalMobs.config.InstrumentalConfigGen;
 import com.Mrbysco.InstrumentalMobs.init.InstrumentalBlocks;
 import com.Mrbysco.InstrumentalMobs.utils.InstrumentHelper;
 
-import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class DrumInstrument extends Item{
@@ -44,64 +34,7 @@ public class DrumInstrument extends Item{
 		sound = soundIn;
 		useDuration = duration;
 	}
-	
-	@Override
-	public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand,
-			EnumFacing facing, float hitX, float hitY, float hitZ) {
-		if(player.isSneaking())
-		{
-			IBlockState iblockstate = worldIn.getBlockState(pos);
-	        Block block = iblockstate.getBlock();
 
-	        if (!block.isReplaceable(worldIn, pos))
-            {
-                pos = pos.offset(facing);
-            }
-
-            ItemStack itemstack = player.getHeldItem(hand);
-
-            if (!itemstack.isEmpty() && player.canPlayerEdit(pos, facing, itemstack) && worldIn.mayPlace(instrumentBlock, pos, false, facing, (Entity)null))
-            {
-                int i = this.getMetadata(itemstack.getMetadata());
-                IBlockState newBlockState = instrumentBlock.getDefaultState().withProperty(DrumBlock.DAMAGE, Math.round(((this.getMaxDamage(itemstack) - this.getDamage(itemstack)) / 16)));
-
-                if (placeBlockAt(itemstack, player, worldIn, pos, facing, hitX, hitY, hitZ, newBlockState))
-                {
-                	newBlockState = worldIn.getBlockState(pos);
-                    SoundType soundtype = newBlockState.getBlock().getSoundType(newBlockState, worldIn, pos, player);
-                    worldIn.playSound(player, pos, soundtype.getPlaceSound(), SoundCategory.BLOCKS, (soundtype.getVolume() + 1.0F) / 2.0F, soundtype.getPitch() * 0.8F);
-                    itemstack.shrink(1);
-                }
-
-                return EnumActionResult.SUCCESS;
-            }
-            else
-            {
-                return EnumActionResult.FAIL;
-            }
-        }
-        else
-        {
-        	return EnumActionResult.FAIL;
-        }
-	}
-	
-	public boolean placeBlockAt(ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ, IBlockState newState)
-    {
-        if (!world.setBlockState(pos, newState, 11)) return false;
-
-        IBlockState state = world.getBlockState(pos);
-        if (state.getBlock() == this.instrumentBlock)
-        {
-            this.instrumentBlock.onBlockPlacedBy(world, pos, state, player, stack);
-
-            if (player instanceof EntityPlayerMP)
-                CriteriaTriggers.PLACED_BLOCK.trigger((EntityPlayerMP)player, pos, stack);
-        }
-
-        return true;
-    }
-	
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
 		ItemStack itemstack = playerIn.getHeldItem(handIn);
