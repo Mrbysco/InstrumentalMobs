27edc86a26de12f163a8786966895d1c12db4998
Mrbysco <Heeybysco@live.nl>
2018-07-31 23:08:26 +0200

    -

--- src/main/java/com/Mrbysco/InstrumentalMobs/Reference.java
+++ src/main/java/com/Mrbysco/InstrumentalMobs/Reference.java
@@ -6,7 +6,7 @@ public class Reference {
 	public static final String MOD_PREFIX = "instrumentalmobs:";
 	public static final String VERSION = "1.0";
 	public static final String ACCEPTED_VERSIONS = "[1.12]";
-	public static final String DEPENDENCIES = "after:trumpetskeleton;";
+	//public static final String DEPENDENCIES = "";
 			
 	public static final String CLIENT_PROXY_CLASS = "com.Mrbysco.InstrumentalMobs.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.Mrbysco.InstrumentalMobs.proxy.ServerProxy";
