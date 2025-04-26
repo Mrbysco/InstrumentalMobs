3b57a642d285127971302492713232d9dbc12001
Mrbysco <Mrbysco@users.noreply.github.com>
2025-04-26 21:42:15 +0200

    Fix spawner modifier (NeoForge)

--- forge/src/main/java/com/mrbysco/instrumentalmobs/modifier/AddRelativeSpawnBiomeModifier.java
+++ forge/src/main/java/com/mrbysco/instrumentalmobs/modifier/AddRelativeSpawnBiomeModifier.java
@@ -4,13 +4,18 @@ import com.mojang.serialization.MapCodec;
 import com.mrbysco.instrumentalmobs.init.InstrumentalModifiers;
 import net.minecraft.core.Holder;
 import net.minecraft.util.random.Weighted;
+import net.minecraft.util.random.WeightedList;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.minecraft.world.level.biome.MobSpawnSettings.SpawnerData;
 import net.neoforged.neoforge.common.world.BiomeModifier;
 import net.neoforged.neoforge.common.world.MobSpawnSettingsBuilder;
 import net.neoforged.neoforge.common.world.ModifiableBiomeInfo.BiomeInfo.Builder;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public record AddRelativeSpawnBiomeModifier(EntityType<?> originalType,
                                             EntityType<?> newType,
@@ -19,12 +24,18 @@ public record AddRelativeSpawnBiomeModifier(EntityType<?> originalType,
 	public void modify(Holder<Biome> biome, Phase phase, Builder builder) {
 		if (phase == Phase.ADD) {
 			MobSpawnSettingsBuilder spawns = builder.getMobSpawnSettings();
-			for (Weighted<SpawnerData> entry : spawns.getSpawner(originalType.getCategory()).getList()) {
+			WeightedList.Builder<MobSpawnSettings.SpawnerData> spawner = spawns.getSpawner(originalType.getCategory());
+			List<Weighted<SpawnerData>> addedSpawns = new ArrayList<>(); //Add the spawns to this list to avoid modifying the original list
+			final List<Weighted<SpawnerData>> spawnsList = spawner.getList();
+			for (Weighted<SpawnerData> entry : spawnsList) {
 				EntityType<?> type = entry.value().type();
 				if (type == originalType) {
-					spawns.addSpawn(type.getCategory(), Math.min(1, entry.weight() / relativeWeight),
-							new SpawnerData(newType, entry.value().minCount(), entry.value().maxCount()));
+					addedSpawns.add(new Weighted<>(new SpawnerData(newType, entry.value().minCount(), entry.value().maxCount()),
+							Math.min(1, entry.weight() / relativeWeight)));
 				}
+			}
+			if (!addedSpawns.isEmpty()) {
+				spawner.addAll(addedSpawns);
 			}
 		}
 	}
